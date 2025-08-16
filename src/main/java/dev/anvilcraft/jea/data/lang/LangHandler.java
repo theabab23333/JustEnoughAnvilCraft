acaf7712c6d090b5f46384f4f96b2a33abf5d31a
theabab23333 <2047477146@qq.com>
2025-08-17 05:18:57 +0800

    init

---
+++ src/main/java/dev/anvilcraft/jea/data/lang/LangHandler.java
@@ -0,0 +1,9 @@
+package dev.anvilcraft.jea.data.lang;
+
+import com.tterrag.registrate.providers.RegistrateLangProvider;
+
+public class LangHandler {
+    public static void init(RegistrateLangProvider provider) {
+
+    }
+}
